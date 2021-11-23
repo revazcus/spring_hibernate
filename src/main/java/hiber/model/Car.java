b0d3f204281a83e97296ece80051a289c7cae846
xRevaZx <revaz1997@mail.ru>
2021-11-23 09:47:47 +0300

    task 2.2.1

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,35 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+
+    public Car() {}
+
+    public Car(String model, int series){
+        this.model = model;
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "{model = '" + model + '\'' +
+                ", series = " + series +
+                '}';
+    }
+}
